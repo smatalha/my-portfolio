21e20a7a4ad3d9ab15cb9be11973c5d1e2c0ef62
smatalha <sma.talha@gmail.com>
2021-01-22 21:29:08 -0500

    added Banner

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,7 +1,32 @@
 import React from 'react'
+import { FaFacebookF, FaTwitter, FaPinterest, FaInstagram, FaMedium } from "react-icons/fa";
 
 const Banner = () => {
     return <header className="header">
+        <div className="container">
+            <div className="row">
+                <div className="col-6">
+                    <div className="header_content">
+                        <div className="header_section">
+                            <ul className="header_ul">
+                                <li><FaFacebookF/></li>
+                                <li><FaTwitter/></li>
+                                <li><FaPinterest/></li>
+                                <li><FaInstagram/></li>
+                                <li><FaMedium/></li>
+                            </ul>
+                            <h1>I am Md Abu Talha Mian</h1>
+                            <p>I,m Talha Professional Full stack Developer looking for new gig</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-6">
+                    <img className="banner_img" src="./images/talha1.png" alt="Talha" />
+
+                </div>
+            </div>
+
+        </div>
     </header>
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,13 +5,17 @@ const Navbar = () => {
         <nav className= "navbar">
             <div className= "container">
                 <div className="navbar__container">
+                    <ul className="navbar__left">
+                            {/* <img src="/images/logo.png" alt="logo" /> */}
+                        <a data-placement="bottom" href="/index" target="_blank" title="Coded by Creative Tim" class="navbar-brand">Md Abu Talha Mian</a>
+                    </ul>
                     <ul className= "navbar_right">
-                        <li> <a hre="">Home</a></li>
-                        <li> <a hre="">About</a></li>
-                        <li> <a hre="">Skills</a></li>
-                        <li> <a hre="">Blog</a></li>
-                        <li> <a hre="">Portfolio</a></li>
-                        <li> <a hre="">Contact</a></li>
+                        <li> <a href="">Home</a></li>
+                        <li> <a href="">About</a></li>
+                        <li> <a href="">Skills</a></li>
+                        <li> <a href="">Blog</a></li>
+                        <li> <a href="">Portfolio</a></li>
+                        <li> <a href="">Contact</a></li>
                     </ul>
                 </div>
             </div>
