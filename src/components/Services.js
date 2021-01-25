b5a066b305fb7607707df25c55cc3496a7380d7c
smatalha <sma.talha@gmail.com>
2021-01-25 06:39:45 -0500

    added services

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import { FaGithub } from "react-icons/fa";
+
 
 const Services = () => {
     return (
@@ -8,10 +10,20 @@ const Services = () => {
                     <div className="common">
                         <h3 className="heading"> SERVICES</h3>
                         <h1 className="main_header">My Services</h1>
-                        <p className="main_context">Lorem Ipsum is simply dummy text of the printing typesetting
-                            industry. simply dummy
+                        <p className="main_context">I'm passionate about what i do. I follow my Dream
                         </p>
                         <div className="common_border"></div>
+                    </div>
+                    <div className="row">
+                        <div className="col-4">
+                            <div className="services_box">
+                                <FaGithub className="common_icons"/>
+                                <div className="services_box_header">Web Development</div>
+                                <div className="services_box_text">
+                                    I do develop charismatic web 
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
